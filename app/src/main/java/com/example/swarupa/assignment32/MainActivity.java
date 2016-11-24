cb1a86b0eefc5b2e47b61de9ec494bc453970575
Swarupa17 <swarupa.khapli@yahoo.in>
2016-11-24 16:41:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/swarupa/assignment32/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.swarupa.assignment32;
+
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt = (Button)findViewById(R.id.button);
+        bt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.setData(ContactsContract.Contacts.CONTENT_URI);
+                        startActivity(intent);
+
+                    }
+                }
+        );
+    }
+}
